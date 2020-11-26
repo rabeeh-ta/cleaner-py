04979beaf66f3d6aacada1427c3fde9934f951cd
rabeeh-ta <rabeehchemnad@gmail.com>
2020-11-26 22:30:34 +0530

    MVP: taking userinput and finding files

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import os
+import argparse
 
+# getting the root file path
 cwd = os.getcwd()
+
+# takking input from the user
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--flutter", help="Deletes the Build folders on every flutter project found", action='store_true')
+parser.add_argument(
+    "--node", help="Deletes the Node-Modules folder on every project found", action='store_true')
+
+args = parser.parse_args()
+
+# search function to find the things to delete
 
 
 def search_func(path, item):
@@ -9,4 +22,15 @@ def search_func(path, item):
             print(folders)
 
 
-search_func(cwd, 'build')
+    # checking the passed argument by the user
+if args.flutter and args.node:
+    print('both clean flutter and node')
+    search_func(cwd, 'build')
+    search_func(cwd, 'node_modules')
+else:
+    if args.flutter:
+        print('clean flutter files')
+        search_func(cwd, 'build')
+    elif args.node:
+        print('clean node files')
+        search_func(cwd, 'node_modules')
