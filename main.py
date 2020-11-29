b9d3e917b8e47f357d848fce9d062296e16b605b
rabeeh-ta <rabeehchemnad@gmail.com>
2020-11-29 14:47:30 +0530

    DOC: added comments.

--- main.py
+++ main.py
@@ -14,31 +14,32 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-total_found = 0  # this is return in killo-bites
+total_found = 0  # calculate the folders found
 
 
 # search function to find the things to delete
 def search_func_root(path, item):
     global total_found
     for folders, sub_folders, files in os.walk(path):
-        if item in sub_folders:
+        if item in sub_folders:  # checks if the flag specific is found in the current folder
             print(f"Found and deleted a {item} directory in {folders}")
-            send2trash.send2trash(folders+"\\"+item)
+            send2trash.send2trash(folders+"\\"+item)  # delete the folder
             total_found += 1
 
     # checking the passed argument by the user
-if args.flutter and args.node:
+if args.flutter and args.node:  # clean both
+    # '\x1b[1;31;40m' coloring thing for cli output
     print('\x1b[1;31;40m' + ' Cleaning both flutter and node ' + '\x1b[0m')
     search_func_root(cwd, 'build')
     search_func_root(cwd, 'node_modules')
 else:
-    if args.flutter:
+    if args.flutter:  # clean flutter- build directorys
         print('\x1b[1;31;40m' + ' Cleaning Flutter files ' + '\x1b[0m')
         search_func_root(cwd, 'build')
-    elif args.node:
+    elif args.node:  # clean node-modules
         print('\x1b[1;31;40m' + ' Cleaning Node files ' + '\x1b[0m')
         search_func_root(cwd, 'node_modules')
 
 
 print('\n' + '\x1b[1;32;40m' +
-      f' Total files found and cleaned: {total_found} ' + '\x1b[0m')
+      f' Total files found and cleaned: {total_found} ' + '\x1b[0m')  # print total number of files deleted
